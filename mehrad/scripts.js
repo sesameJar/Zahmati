5a1638accab48eef08eb6d93deaa3b40194b0fe9
mehrad <mehrad.kavian@gmail.com>
2012-09-07 22:18:49 +0430

    there is an error and i can't fix it

--- mehrad/scripts.js
+++ mehrad/scripts.js
@@ -1,8 +1,24 @@
-$(
+$( function () {
 	var menuLists 	  = $('header.body nav menu li'),
 		contentTarget = $('article.content div.bio');
 
-	menuLists.each.click(alert(1));
+	menuLists.each( function () {
+
+		var that = this;
+
+		setTimeout(function(){
+			
+			$(that).click(function(){
+				
+				$(that).addClass('active');
+				contentTarget.animate({
+					height:200
+				},300,contentTarget.css({display:'block'}))
+			});
+		},800);
+		
 
 
-);
+		
+	});
+});
