fa9041d47c3cd7734fdb7a7b644b7fb166e1d709
mehrad <mehrad.kavian@gmail.com>
2012-09-09 19:40:06 +0430

    fixed the bug

--- mehrad/scripts.js
+++ mehrad/scripts.js
@@ -1,30 +1,44 @@
 $( function () {
-	var menuLists 	  = $('header.body nav menu li'),
-		contentTarget = $('article.content div');
+	var menuLists 	  = $(' header.body nav menu li ' ),
+		contentTarget = $(' article.content div ');
 
 	menuLists.each( function () {
 
 		var that = this;
 
-		$(that).click(function(){
-			if(contentTarget.css('display')=='block'){
-				setTimeout(function(){
-				contentTarget.eq($(that).index()).animate({
-					height:0
-				},300,contentTarget.eq($(that).index()).css({display:'none'}))
-			},100)
+		$(that).click( function() {
+
+			if( contentTarget.eq( $(that).index() ).css( 'display' ) == 'none' ){
+				$(that).addClass('active');
+
+				contentTarget.eq( $(that).index() ).animate({
+
+					display : 'block'
+
+				},0,'linear',function(){
+
+					contentTarget.eq( $(that).index() ).animate( { 
+
+						height : 300
+
+					},300)
+				})
+
 			}
+
 			else{
-				setTimeout(function(){
-				contentTarget.eq($(that).index()).animate({
-					height:200
-				},300,contentTarget.eq($(that).index()).css({display:'block'}))
-			},100)
+				$(that).removeClass('active');
+				contentTarget.eq( $(that).index() ).animate({
+
+					height:0
+
+				},300,'linear', function() {
+
+					contentTarget.eq( $(that).index() ).css({ display : 'none' });
+
+				});
+
 			}
-		})
-		
-
-
-		
+		})	
 	});
 });
