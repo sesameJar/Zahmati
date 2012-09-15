c40e34bc19b067115333524c4865277950b30ec9
mehrad <mehrad.kavian@gmail.com>
2012-09-16 01:24:38 +0430

    adding dynamic for thumbnalis

--- mehrad/index.php
+++ mehrad/index.php
@@ -45,12 +45,22 @@
 				<div id="prearrow"></div>
 				<div class="little-slide">
 					<ul>
-						<li style="background-image:url(../images/thumbnail/logo/typographi.jpg)"></li>
-						<li style="background-image:url(../images/thumbnail/logo/picha-fast-food.jpg)"></li>
-						<li style="background-image:url(../images/thumbnail/logo/hotel-darvishi.jpg)"></li>
-						<li style="background-image:url(../images/thumbnail/logo/faramehr-advertising.jpg)"></li>
-						<li style="background-image:url(../images/thumbnail/logo/bankiha-24-.jpg)"></li>
-						<li style="background-image:url(../images/thumbnail/logo/atolie-sarv.jpg)"></li>
+						<?php 
+							$logo_thumb_images_path = "../images/thumbnail/logo" ;
+							$logo_images            = "../images/logo" ;
+							$logo_thumb_images      = scandir($logo_thumb_images_path);
+
+							foreach( $logo_thumb_images as $logo_thumb_image ) {
+
+								$img_type = strtolower( end( explode( '.' , $logo_thumb_image ) ) );
+
+								if( $img_type == 'jpg' || $img_type == 'png' || $img_type == 'gif' ) {
+
+									echo "<li><a href='$logo_images/$logo_thumb_image'><img src=\"$logo_thumb_images_path/$logo_thumb_image\" /></a></li>";
+								}
+
+							}
+						?>
 					</ul>
 				</div>
 				<div id="nextarrow"></div>
