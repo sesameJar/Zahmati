0544c7397a3116afd72a5f1f23a3b0b57c3ee6c1
mehrad <mehrad.kavian@gmail.com>
2012-09-17 11:45:23 +0430

    adding style for text inputs

--- mehrad/index.php
+++ mehrad/index.php
@@ -71,9 +71,9 @@
 		<div class="contact-us">
 			<form action="sendmail.php" id="contact-from" method="post">
 				<label for="name">Name :</label>
-				<input type="text" name="name" id="name" placeholder="Type Your/Company Name!" />
+				<input type="text" class="text" name="name" id="name" placeholder="Type Your/Company Name!" />
 				<label for="email">E-mail :</label>
-				<input type="text" name="email" id="email" placeholder="Type Your Email!" />
+				<input type="text" class="text" name="email" id="email" placeholder="Type Your Email!" />
 				<label for="message">Message :</label>
 				<textarea name="message" id="message" cols="30" rows="10" placeholder="Share Your Id !"></textarea>
 			</form>
