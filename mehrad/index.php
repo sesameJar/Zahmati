41e09502469826732979f1ec7d19e9561d5cc13a
mehrad <mehrad.kavian@gmail.com>
2012-09-22 21:42:43 +0330

    starting to seprat files

--- mehrad/index.php
+++ mehrad/index.php
@@ -1,37 +1,8 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8" />
-	<title>زحمتی</title>
-	<link rel="stylesheet" type="text/css" href="reset.css" />
-	<link rel="stylesheet" type="text/css" href="style.css" />
-	<link rel="shortcut icon" href='../favicon.ico' />
-	<script type="text/javascript" src="../zepto.min.js"></script>
-	<script type="text/javascript" src="scripts.js"></script>
+<?php
 
-	<!--[if lt IE 9]>
-     <script src="../html5shiv.js"></script>
-	<![endif]-->
-</head>
-<body>
-
-<div class="main-container">
-	<header class="body">
-		<nav>
-			<menu>
-				<li><a href="#">biography .</a></li>
-				<li><a href="#">about graphic .</a></li>
-				<li><a href="#">poster .</a></li>
-				<li><a href="#">logo .</a></li>
-				<li><a href="#">book & magazine .</a></li>
-				<li><a href="#">others .</a></li>
-				<li><a href="#">connect .</a></li>
-				<li class="clear"></li>
-			</menu>
-		</nav>
-		<section class="logo"><img src="../images/zahmati-logo.png" alt="zahmati" /></section>
-		<div class="clear"></div>
-	</header>
+	include './header.php';
+	
+?>
 	<article class="content">
 		<div class="bio">
 			
