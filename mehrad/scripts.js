55aaf6a58f8a90598b6f73ae8c8cfa3a3e5b8506
mehrad <mehrad.kavian@gmail.com>
2012-09-24 00:33:47 +0330

    mistakes in spaces

--- mehrad/scripts.js
+++ mehrad/scripts.js
@@ -1,13 +1,13 @@
 $( function () {
 
-	var menuLists 	    = $(' header.body nav menu li ' ),
-		contentTarget   = $(' article.content > div '),
-		logoLittleSlide = $(' article.content  div div.little div.little-slide '),
-		logoSlidePre    = $(' article.content  div div.little div#prearrow '),
-		logoSlideNext   = $(' article.content  div div.little div#nextarrow '),
-		logoTrain       = $(' article.content  div ul '),
-		logoLists       = $(' article.content  div ul li a '),
-		loadPlace       = $(' #loadplace ');
+	var menuLists 	    = $('header.body nav menu li'),
+		contentTarget   = $('article.content > div'),
+		logoLittleSlide = $('article.content  div div.little div.little-slide'),
+		logoSlidePre    = $('#prearrow'),
+		logoSlideNext   = $('#nextarrow'),
+		logoTrain       = $('article.content  div ul'),
+		logoLists       = $('article.content  div ul li a'),
+		loadPlace       = $('#loadplace');
 
 
 	logoSlideNext.click( function () {
