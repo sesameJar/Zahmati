98e89ea68baa692b9ea88bb7411c6ece9f70fc52
mehrad <mehrad.kavian@gmail.com>
2012-09-24 00:13:06 +0330

    i omitted the semi ajax form index

--- mehrad/scripts.js
+++ mehrad/scripts.js
@@ -9,46 +9,6 @@ $( function () {
 		logoLists       = $(' article.content  div.logo ul li a '),
 		loadPlace       = $(' article.content  div.logo div#loadplace img ');
 
-	menuLists.click( function () {
-
-		var that         = this,
-			targetHeight = $(this).css('height');
-
-			if( contentTarget.eq( $(that).index() ).css( 'display' ) == 'none' ){
-
-				$(that).addClass('active');
-
-				contentTarget.eq( $(that).index() ).animate({
-
-					display : 'block'
-
-				},0,'linear',function(){
-
-					contentTarget.eq( $(that).index() ).animate( { 
-
-						'min-height' : targetHeight
-
-					},300)
-				})
-
-			}
-
-			else{
-				$(that).removeClass('active');
-
-				contentTarget.eq( $(that).index() ).animate({
-
-					'min-height' : 0
-
-				},300,'linear', function() {
-
-					contentTarget.eq( $(that).index() ).css({ display : 'none' });
-
-				});
-
-			}
-		
-	});
 
 	logoSlideNext.click( function () {
 
