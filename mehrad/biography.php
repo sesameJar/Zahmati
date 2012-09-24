996201e0bf7882f941bf418520e771876d99093f
mehrad <mehrad.kavian@gmail.com>
2012-09-25 01:59:42 +0330

    lots of changes and  working on complete ajax

---
+++ mehrad/biography.php
@@ -0,0 +1,9 @@
+
+
+<div class="biography">
+			
+	<p>	Born on winter of 1990 in Ghoochan and student of Bachelor degree.</p>
+	<p>	<span>Resume</span> : Arman printing office, Eima Digital Printing office, Faramehr Commercial Center, Chief Manager of Ghoochan Graphic Convention, cooprate with Mashhad Bus Organization, Cooprate With Partow Convention.</p>
+	<p>	<span>Exhibition</span> : Indivdual Poster Named "Navar Maghzi" at Kowsar Gallery.</p>
+	
+</div>
--- mehrad/scripts.js
+++ mehrad/scripts.js
@@ -6,50 +6,39 @@ $( function () {
 		logoSlidePre    = $('#prearrow'),
 		logoSlideNext   = $('#nextarrow'),
 		logoTrain       = $('article.content  div ul'),
-		logoLists       = $('article.content  div ul li a'),
-		loadPlace       = $('#loadplace');
+		logoList       	= $('article.content  div ul li'),
+		logoListLink    = $('article.content  div ul li a'),
+		loadPlace       = $('#loadplace'),
+		logoListLen		= (logoList.length)/5,
+		currentSession  = 0;
 
+	(nextSession = function(n){
+
+		if(n>logoListLen-1) n=0;
+
+		if(n<0) n=logoListLen-1;
+
+		logoTrain.css({top: (-260*n)+'px' });
+
+		currentSession=n;
+	})(0); 
 
 	logoSlideNext.click( function () {
 
-		logoTrain.animate( {
-
-			top : -383
-
-		},400);
-
+		nextSession(currentSession+1);
 	});
 
 	logoSlidePre.click( function () {
 
-		logoTrain.animate({
-
-			top : 0
-
-		},400)
-
+		nextSession(currentSession-1);
 	});
 
-	logoLists.click(function(){
+	logoListLink.click(function(){
 
 		var thumbnailHref = $(this).attr('href');
 
 		$('#loadplace img').attr('src',thumbnailHref);
 
 		return false;
-
-	})
-
-
+	});
 });
-
-
-
-
-
-
-
-
-
-
-
